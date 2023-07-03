1b6d794058a952b558ddecc735c0f5a1887cab0f
Lucas <lucasferrazl@hotmail.com>
2023-07-03 17:06:40 -0300

    Add basic config to redux store

---
+++ src/store/gameSlice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const gameSlice = createSlice({
+    name: "game",
+    initialState: {
+        // ...
+    },
+    reducers: {
+        // ...
+    },
+});
+
+export const { actions, reducer } = gameSlice;
+export default reducer;
