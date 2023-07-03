a8098a13c85593ae37389b49a86c0c11ee3757a0
Lucas <lucasferrazl@hotmail.com>
2023-07-03 17:49:37 -0300

    Define game's initial state

--- src/store/gameSlice.ts
+++ src/store/gameSlice.ts
@@ -1,10 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface Dot {
+    color: string;
+    position: {
+        x: number;
+        y: number;
+    };
+}
+
+interface GameState {
+    gameBoard: Dot[][];
+    score: number;
+    isGameStarted: boolean;
+}
+
+const initialState: GameState = {
+    gameBoard: [],
+    score: 0,
+    isGameStarted: false,
+};
+    
+
 const gameSlice = createSlice({
     name: "game",
-    initialState: {
-        // ...
-    },
+    initialState: initialState,
     reducers: {
         // ...
     },
